bc7213e2efe8a4de54e3957f056df294805f1e55
Shafi <44125051+m-2127@users.noreply.github.com>
2021-12-01 15:31:14 +0530

    An efficient (O(logn)/O(n)) program to find F mod n

---
+++ FibonacciHuge.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class FibonacciHuge {
+    private static long getFibonacciHugeNaive(long n, long m) {
+        if (n <= 1)
+            return n;
+
+        long previous = 0;
+        long current  = 1;
+		long divisorLength = 1;
+		
+		ArrayList<Long> list = new ArrayList<Long>();
+		list.add(0L);
+		list.add(1L);
+
+        for (long i = 0; i < n - 1; ++i) {
+			
+			if (i < 2){
+				long tmp_previous = previous;
+				previous = current;
+				current = tmp_previous + current; // i + 2 th fibonacci number
+			
+				list.add(current % m);
+			}
+            else{
+				if((list.get(list.size() - 2) == 0) && (list.get(list.size() - 1) == 1)){
+					divisorLength = list.size() -2;
+					//System.out.println();
+					return list.get((int)(n % divisorLength));
+				}
+				else{
+					list.add((list.get(list.size() - 2) + (list.get(list.size() - 1))) % m);
+				}
+			}
+			
+			/*if ( (i > 1) && (list.get(list.size() - 2) == 0) && (list.get(list.size() - 1) == 1)) {
+				System.out.println(i);
+				divisorLength = list.size() - 3;
+				break;
+			}
+			else if (i == (n - 2)){
+				return current % m;
+			}*/
+        }
+		
+		return list.get(list.size() - 1);
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        long m = scanner.nextLong();
+        System.out.println(getFibonacciHugeNaive(n, m));
+    }
+}

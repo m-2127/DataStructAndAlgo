423dd5d7abc7df6db456b1e59b24e337ee5ece18
Shafi <44125051+m-2127@users.noreply.github.com>
2021-12-25 09:39:42 +0530

    Knapsack solved by reducing complexity

--- FractionalKnapsack.java
+++ FractionalKnapsack.java
@@ -4,46 +4,22 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class FractionalKnapsack {
-    private static double getOptimalValue(double capacity, double[] values, double[] weights) {
-        //write your code here
-		
-		//double value, weight, currentValue, currentWeight;
-		//double[] unitValues = new double[n];
+    private static double getOptimalValue(double capacity, double[][] values, double[] weights) {
 		
 		DecimalFormat df_obj = new DecimalFormat("#.####");
 		
-        /*for (int i = 0; i < (values.length-1); i++) {
-            currentValue = values[i+1];
-            currentWeight = weights[i+1];
-			value = values[i];
-			weight = weights[i];
-			
-			for(int j = i+1; i > 0 ; i--){
-				if ((currentValue/currentWeight) < (value/weight)){
-					double tmpValue = values[i]; 
-					double tmpWeight = weights[i];
-					values[i] = values[i+1];
-					weights[i] = weights[i+1];
-					values[i+1] = tmpValue;
-					weights[i+1] = tmpWeight;
-				}
-			}
-			//unitValues[i] = df_obj.format(value/weight);
-        }*/
-		
-		//Arrays.sort(unitValues, Collections.reverseOrder());
 		double remainingCapacity = capacity;
 		double finalValue = 0.0000;
 		
 		for (int i = values.length-1; i >= 0; i--) {
 			
-			if(remainingCapacity < weights[i]){
-				finalValue += Double.parseDouble(df_obj.format((values[i]/weights[i])*remainingCapacity));
+			if(remainingCapacity < (values[i][1])){
+				finalValue += Double.parseDouble(df_obj.format((values[i][0])*remainingCapacity));
 				return finalValue;
 			}
 			else{
-				finalValue += Double.parseDouble(df_obj.format((values[i]/weights[i])*weights[i]));
-				remainingCapacity -= weights[i];
+				finalValue += Double.parseDouble(df_obj.format((values[i][0])*(values[i][1])));
+				remainingCapacity -= (values[i][1]);
 			}
         }
 
@@ -55,33 +31,34 @@ public class FractionalKnapsack {
         int n = scanner.nextInt();
         double capacity = (double)scanner.nextInt();
         double[] values = new double[n];
+		double[][] valuePerUnit = new double[n][2];
         double[] weights = new double[n];
-		double value, weight, currentValue, currentWeight;
+		double tmpValue, tmpWeight, currentValue, currentWeight, x,y;
 		
         for (int i = 0; i < n; i++) {
-            values[i] = (double)scanner.nextInt();
-            weights[i] = (double)scanner.nextInt();
+            tmpValue = (double)scanner.nextInt();
+            tmpWeight = (double)scanner.nextInt();
+			weights[i] = tmpWeight;
 			
-			if(i==0){
-				continue;
-			}
+			double[] valAndWeight = new double[2];
+			valAndWeight[0] = tmpValue/tmpWeight;
+			valAndWeight[1] = tmpWeight;
 			
-			currentValue = values[i];
-            currentWeight = weights[i];
-			value = values[i-1];
-			weight = weights[i-1];
-			
-			for(int j = i; i > 0 ; i--){
-				if ((currentValue/currentWeight) < (value/weight)){
-					double tmpValue = values[i]; 
-					double tmpWeight = weights[i];
-					values[i] = values[i-1];
-					weights[i] = weights[i-1];
-					values[i-1] = tmpValue;
-					weights[i-1] = tmpWeight;
-				}
-			}
+			valuePerUnit[i] = valAndWeight;
         }
-        System.out.println(getOptimalValue(capacity, values, weights));
+		
+		//Arrays.sort(valuePerUnit);
+		Arrays.sort(valuePerUnit, (a, b) -> Double.compare(a[0], b[0]));
+		
+		for (int i = 0; i < valuePerUnit.length; i++) {
+  
+            // accessing each element of array
+            //x = valuePerUnit[i][0];
+			//y = valuePerUnit[i][1];
+            //System.out.print(x + " - " + y + " ");
+        }
+		//System.out.print("\n");
+		
+        System.out.println(getOptimalValue(capacity, valuePerUnit, weights));
     }
 } 
